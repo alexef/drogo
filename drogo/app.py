e650a945f88e8f22cac707cf04268565acf98805
Mihai Zamfir <mihai.zamfir@eaudeweb.ro>
2015-01-08 12:30:14 +0200

    update requirements and prepare testsuite

--- drogo/app.py
+++ drogo/app.py
@@ -3,9 +3,12 @@ from drogo.models import db
 from drogo.views import views
 
 
-def create_app():
+def create_app(config={}):
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_pyfile('settings.py')
+    if not config:
+        app.config.from_pyfile('settings.py')
+    else:
+        app.config.update(config)
     db.init_app(app)
 
     app.register_blueprint(views)
