bb77e47996361adb29106005f4201611c88edf0a
Alex Eftimie <alex@eftimie.ro>
2015-03-12 14:50:53 +0200

    Fix bad merge.

--- drogo/auth.py
+++ drogo/auth.py
@@ -19,11 +19,11 @@ def ldap_fetch(name=None, passwd=None):
         flash('Could not login: {}'.format(e), 'error')
         return None
 
-    existing_user = User.query.filter(User.ldap_name == user_id).first()
+    existing_user = User.query.filter(User.ldap_username == user_id).first()
     if existing_user:
         return existing_user
 
-    new_user = User(ldap_name=user_id, full_name=user_id)
+    new_user = User(ldap_username=user_id, full_name=user_id)
     db.session.add(new_user)
     db.session.commit()
     return new_user
