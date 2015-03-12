b694198e624034e9bb10c49afee667bf063a9c5a
Alex Eftimie <alex@eftimie.ro>
2015-03-12 13:12:41 +0200

    [auth] Show error if i couldn't login

--- drogo/auth.py
+++ drogo/auth.py
@@ -1,5 +1,5 @@
 import simpleldap
-from flask import current_app
+from flask import current_app, flash
 from drogo.models import db, User
 
 
@@ -7,18 +7,22 @@ def ldap_fetch(name=None, passwd=None):
     ldapsrv = current_app.config['LDAPSRV']
     basedn = current_app.config['BASEDN']
 
+    user_id = None
     try:
         if name is not None and passwd is not None:
             l = simpleldap.Connection(ldapsrv,
-                dn='uid={0},{1}'.format(name, basedn), password=passwd)
+                                      dn='uid={0},{1}'.format(name, basedn),
+                                      password=passwd)
             r = l.search('uid={0}'.format(name), base_dn=basedn)
-    except:
+            user_id = r[0]['uid'][0]
+    except Exception as e:
+        flash('Could not login: {}'.format(e), 'error')
         return None
 
-    existing_user = User.query.filter(User.ldap_name == r[0]['uid'][0]).first()
+    existing_user = User.query.filter(User.ldap_name == user_id).first()
     if existing_user:
         return existing_user
-    new_user = User(ldap_name=r[0]['uid'][0], full_name=r[0]['uid'][0])
+    new_user = User(ldap_name=user_id, full_name=user_id)
     db.session.add(new_user)
     db.session.commit()
     return new_user
